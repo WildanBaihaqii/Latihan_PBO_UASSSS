e8f1f67ffb75c34fb568fce506708f0e411657f0
WildanBaihaqii <haaaqyyyy@example.com>
2026-06-25 22:12:14 +0700

    [Tahap 5] - Overiding

--- class/RentalMotor.php
+++ class/RentalMotor.php
@@ -19,28 +19,31 @@ class RentalMotor extends Kendaraan{
         <strong>Kapasitas Mesin :</strong> {$this->kapasitasCC} CC<br>
         <strong>Helm Box : </strong> {$this->termasukHelmbox}";
     }
-    // pembantu query statis
-    public static function getDaftarMotor($db){
-        $query = "SELECT id_kendaraan, no_plat, merk_model, hari_sewa, tarif_per_hari, kapasitas_cc, termasuk_helmbox
-                  FROM tabel_kendaraan
-                  WHERE jenis_kendaraan = 'Motor'";
-        $result = $db->query($query);
-        
-        $listMotor = [];
-        if($result && $result->num_rows > 0){
-            while($row = $result->fetch_assoc()){
-                $listMotor[] = new RentalMotor(
-                    $row['id_kendaraan'],
-                    $row['no_plat'],
-                    $row['merk_model'],
-                    $row['hari_sewa'],
-                    $row['tarif_per_hari'],
-                    $row['kapasitas_cc'],
-                    $row['termasuk_helmbox']
-                );
-            }
+    public static function getDaftarMotor($db) {
+
+    $query = "SELECT id_kendaraan, no_plat, merk_model, hari_sewa, tarif_per_hari, kapasitas_cc, termasuk_helm_box 
+              FROM tabel_kendaraan 
+              WHERE jenis_kendaraan = 'Motor'";
+              
+    $result = $db->query($query);
+    
+    $listMotor = [];
+    
+    if ($result && $result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $listMotor[] = new RentalMotor(
+                $row['id_kendaraan'],
+                $row['no_plat'],
+                $row['merk_model'],
+                $row['hari_sewa'],
+                $row['tarif_per_hari'],
+                $row['kapasitas_cc'],
+                $row['termasuk_helm_box'] 
+            );
         }
-        return $listMotor;
     }
+    
+    return $listMotor;
+}
 }
 ?>
