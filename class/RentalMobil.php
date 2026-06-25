5085bec82ffae0cc7d19e81be2402666342bd38e
WildanBaihaqii <haaaqyyyy@example.com>
2026-06-25 21:20:41 +0700

    [Tahap 4] - Membuat Sublclass Dari Induk Kendaraan Menjadi 3 Sublclass

--- class/RentalMobil.php
+++ class/RentalMobil.php
@@ -1,8 +1,11 @@
 <?php
 require_once 'src/Kendaraan.php';
 class RentalMobil extends Kendaraan{
+    // Atribut
     private $kapasitasPenumpang;
     private $asuransiId;
+
+    // Construct
     public function __construct($id, $plat, $merk, $hari, $tarif, $kapasitas, $asuransi){
         parent::__construct($id, $plat, $merk, $hari, $tarif);
         $this->kapasitasPenumpang = $kapasitas;
