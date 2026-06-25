89ae98eb55ba4649d2436b20900afed738e5e18f
WildanBaihaqii <haaaqyyyy@example.com>
2026-06-25 21:46:19 +0700

    [Tahap 5] - Membuat Overiding Total Sewa Motor & Spesiufik Unil Motor

--- class/RentalMotor.php
+++ class/RentalMotor.php
@@ -10,5 +10,37 @@ class RentalMotor extends Kendaraan{
         $this->kapasitasCC = $cc;
         $this->termasukHelmbox = $helmBox;
     }
+    // Tahap 5 
+    public function hitungTotalSewa(){
+        return ($this->hariSewa * $this->tarifPerhHari);
+    }
+    public function tampilkanSpesifikasi(){
+        return "
+        <strong>Kapasitas Mesin :</strong> {$this->kapasitasCC} CC<br>
+        <strong>Helm Box : </strong> {$this->termasukHelmbox}";
+    }
+    // pembantu query statis
+    public static function getDaftarMotor($db){
+        $query = "SELECT id_kendaraan, no_plat, merk_model, hari_sewa, tarif_per_hari, kapasitas_cc, termasuk_helmbox
+                  FROM tabel_kendaraan
+                  WHERE jenis_kendaraan = 'Motor'";
+        $result = $db->query($query);
+        
+        $listMotor = [];
+        if($result && $result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                $listMotor[] = new RentalMotor(
+                    $row['id_kendaraan'],
+                    $row['no_plat'],
+                    $row['merk_model'],
+                    $row['hari_sewa'],
+                    $row['tarif_per_hari'],
+                    $row['kapasitas_cc'],
+                    $row['termasuk_helmbox']
+                );
+            }
+        }
+        return $listMotor;
+    }
 }
 ?>
