5a1a80f68aebdec52f2323571317dd0b400151ce
WildanBaihaqii <haaaqyyyy@example.com>
2026-06-25 21:43:08 +0700

    [Tahap 5] - Membuat Overiding Total Sewa Mobil & Spesiufik Unil Mobil

--- class/RentalMobil.php
+++ class/RentalMobil.php
@@ -1,25 +1,46 @@
 <?php
 require_once 'src/Kendaraan.php';
 class RentalMobil extends Kendaraan{
-    // Atribut
     private $kapasitasPenumpang;
     private $asuransiId;
-
-    // Construct
     public function __construct($id, $plat, $merk, $hari, $tarif, $kapasitas, $asuransi){
         parent::__construct($id, $plat, $merk, $hari, $tarif);
         $this->kapasitasPenumpang = $kapasitas;
         $this->asuransiId = $asuransi;
     }
-    //Tahap 5 : Overiding Hitung Total Sewa Mobil
+    //Tahap 5 
     public function hitungTotalSewa(){
         return ($this->hariSewa * $this->tarifPerhHari) + 150000;
     }
-    // Tahap 5 : Tampilan Spesifik Unik Mobil
+    // Tahap 5 
     public function tampilkanSpesifikasi(){
         return "
         <strong>Kapasitas Penumpang :</strong> {$this->kapasitasPenumpang} Orang<br>
         <strong>Asuransi :</strong> {$this->asuransiId}";
     }
+    // Pembantu query static
+    public static function getDaftarMobil($db) {
+        $query = "SELECT id_kendaraan, no_plat, merk_model, hari_sewa, tarif_per_hari, kapasitas_penumpang, asuransi_id 
+                  FROM tabel_kendaraan 
+                  WHERE jenis_kendaraan = 'Mobil'";
+        $result = $db->query($query);
+        $listMobil = [];
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $listMobil[] = new RentalMobil(
+                    $row['id_kendaraan'],
+                    $row['no_plat'],
+                    $row['merk_model'],
+                    $row['hari_sewa'],
+                    $row['tarif_per_hari'],
+                    $row['kapasitas_penumpang'],
+                    $row['asuransi_id']
+                );
+            }
+        }
+        
+        return $listMobil;
+    }
+
 }
 ?>
